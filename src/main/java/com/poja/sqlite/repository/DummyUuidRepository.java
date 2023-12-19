1d942c5f799158794fece7965b32a547f44a9e00
Andriamahaly Loiqua <hei.loiqua@gmail.com>
2023-12-19 11:05:34 +0300

    add all project poja-sqlite

---
+++ src/main/java/com/poja/sqlite/repository/DummyUuidRepository.java
@@ -0,0 +1,15 @@
+package com.poja.sqlite.repository;
+
+import com.poja.sqlite.PojaGenerated;
+import com.poja.sqlite.repository.model.DummyUuid;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@PojaGenerated
+@Repository
+public interface DummyUuidRepository extends JpaRepository<DummyUuid, String> {
+
+  @Override
+  Optional<DummyUuid> findById(String id);
+}
